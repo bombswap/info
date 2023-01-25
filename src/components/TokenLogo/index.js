2f4cb0d0841a4c6b7dfa4f2209fd473d47c66334
Aaron <aaron@1coin.io>
2023-01-25 09:25:32 +0000

    token icons

--- src/components/TokenLogo/index.js
+++ src/components/TokenLogo/index.js
@@ -71,9 +71,9 @@ export default function TokenLogo({ address, header = false, size = '24px', ...r
     )
   }
 
-  const path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${isAddress(
+  const path = `https://raw.githubusercontent.com/bombmoney/bomb-assets/master/bombswap/${isAddress(
     address
-  )}/logo.png`
+  ).toLowerCase()}.png`
 
   return (
     <Inline>
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -8,9 +8,9 @@ import copy from 'copy-to-clipboard'
 export function useColor(tokenAddress, token) {
   const [color, setColor] = useState('#2172E5')
   if (tokenAddress) {
-    const path = `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${isAddress(
-      tokenAddress
-    )}/logo.png`
+    const path = `https://raw.githubusercontent.com/bombmoney/bomb-assets/master/bombswap/${isAddress(tokenAddress)
+      .toString()
+      .toLowerCase()}.png`
     if (path) {
       Vibrant.from(path).getPalette((err, palette) => {
         if (palette && palette.Vibrant) {
